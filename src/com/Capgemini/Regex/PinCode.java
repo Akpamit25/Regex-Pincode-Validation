d97b1004854c6e48c89ca050eb9be9ee37151613
Amit Kumar <amitnitsri25@gmail.com>
2020-11-01 10:35:00 +0530

    UC1 : Create Regex Pattern To Validate PIN code 400088

--- src/com/Capgemini/Regex/PinCode.java
+++ src/com/Capgemini/Regex/PinCode.java
@@ -1,12 +1,31 @@
 package com.Capgemini.Regex;
 
-import java.util.regex.Pattern;
 import java.util.regex.*;
+
 import java.util.*;
 
 public class PinCode {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Pin Code Validation");
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Pincode To Validate");
+		String pin = sc.nextLine();
+		PinCode p = new PinCode();
+		p.checkPinCode(pin);
+		sc.close();
+	}
+
+	public void checkPinCode(String str) {
+		String regex = "^[1-9]?[0-9]{5}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		boolean valid = matcher.matches();
+		if (valid == true) {
+			System.out.println("Valid Pin Code");
+		} else if (valid == false) {
+			System.out.println("Invalid Pin Code");
+		}
+
 	}
 }
