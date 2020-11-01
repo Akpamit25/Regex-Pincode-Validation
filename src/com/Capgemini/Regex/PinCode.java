f79064c949e651a8d556bead9a86156a77d40767
Amit Kumar <amitnitsri25@gmail.com>
2020-11-01 10:29:50 +0530

    Welcome To Master Branch Of Pin Code Validation

---
+++ src/com/Capgemini/Regex/PinCode.java
@@ -0,0 +1,12 @@
+package com.Capgemini.Regex;
+
+import java.util.regex.Pattern;
+import java.util.regex.*;
+import java.util.*;
+
+public class PinCode {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Pin Code Validation");
+	}
+}
